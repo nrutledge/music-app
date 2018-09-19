f2521f5494956748534143d93a4a937322da2063
Neil Rutledge <neilrutledgephoto@gmail.com>
2018-09-19 08:54:35 -0400

    Switching order of synth and piano

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,8 +10,8 @@ export default class App extends Component {
     return (
       <div className="app">
         <Instrument audioCtx={audioCtx} keyMappings={drums} name={'Rock Drums (ง\'̀-\'́)ง'} />
-        <Instrument audioCtx={audioCtx} keyMappings={synth} name={'Some Synth Thing?'} />
         <Instrument audioCtx={audioCtx} keyMappings={piano} name={'~(˘▾˘~) Low Quality Piano'} />
+        <Instrument audioCtx={audioCtx} keyMappings={synth} name={'Some Synth Thing?'} />
       </div>
     );
   }
