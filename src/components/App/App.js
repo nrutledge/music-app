42baf954bc12b2e152d49e91ab4a604add70adb2
Neil Rutledge <neilrutledgephoto@gmail.com>
2019-03-18 22:51:20 -0400

    Removed React router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
 import StudioContainer from '../../containers/StudioContainer';
 
 export default () => {
   return (
     <div className="container" style={{ height: '100%' }}>
-      <BrowserRouter>
-          <Route exact path="/" component={StudioContainer} />
-      </BrowserRouter>
+      <StudioContainer />
     </div>
   );
 }
